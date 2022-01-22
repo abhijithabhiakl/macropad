172f47986e44784959ee68a95986753d8195925c
abhijithabhiakl <abhiabhijith367@gmail.com>
2022-01-22 22:15:18 +0530

    start ignoring boot.py

--- boot.py
+++
@@ -1,5 +0,0 @@
-import supervisor
-
-supervisor.set_next_stack_limit(4096 + 4096)
-
-#a change made after adding gitignore
--- code.py
+++ code.py
@@ -1,5 +1,4 @@
 print("Starting")
-#a change made after adding gitignore
 import board
 import digitalio
 from kmk.kmk_keyboard import KMKKeyboard
