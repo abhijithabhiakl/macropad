9cc27c7caf0247b139f6ace511cfe7be8a0af6e4
Abhijithabhi <abhiabhijith367@gmail.com>
2022-01-22 20:57:16 +0530

    initialC_abhi's MP code_oldKMK

---
+++ OLD/v1/kmk/extensions/cled.py
@@ -0,0 +1,35 @@
+import time
+
+import board
+import pwmio
+from digitalio import DigitalInOut, Direction
+
+pwm_leds = board.GP25
+pwm = pwmio.PWMOut(pwm_leds, frequency=1000, duty_cycle=0)
+
+# digital LEDs connected on D2
+digital_leds = DigitalInOut(board.GP0)
+digital_leds.direction = Direction.OUTPUT
+brightness = 0  
+fade_amount = 1285 
+counter = 0  
+
+while True:
+    pwm.duty_cycle = brightness
+
+    brightness = brightness + fade_amount
+
+    print(brightness)
+    if brightness <= 0:
+        fade_amount = -fade_amount
+        counter += 1
+    elif brightness >= 65535:
+        fade_amount = -fade_amount
+        counter += 1
+    time.sleep(.015)
+
+    if counter % 4 == 0:
+        digital_leds.value = True
+    else:
+        digital_leds.value = False
+print("LED working")
---
+++ code.py
@@ -0,0 +1,62 @@
+print("Starting")
+
+import board
+import digitalio
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.matrix import DiodeOrientation
+from kmk.extensions.led import LED
+from kmk.modules.encoder import EncoderHandler
+
+#led = digitalio.DigitalInOut(board.LED)
+#led.direction = digitalio.Direction.OUTPUT
+
+keyboard = KMKKeyboard()
+keyboard.debug_enabled = False
+led_red = LED(led_pin=board.GP9,
+    brightness_step=5,
+    brightness_limit=100,
+    breathe_center=2.75,
+    animation_mode=3,
+    animation_speed=0.14,
+    val=100,)
+led_blue = LED(led_pin=board.GP10,
+    brightness_step=5,
+    brightness_limit=100,
+    breathe_center=2.75,
+    animation_mode=3,
+    animation_speed=0.21,
+    val=100,)
+led_green = LED(led_pin=board.GP12,
+    brightness_step=5,
+    brightness_limit=0,
+    breathe_center=2.75,
+    animation_mode=3,
+    animation_speed=0.07,
+    val=100,)
+keyboard.extensions.append(led_red)
+keyboard.extensions.append(led_blue)
+keyboard.extensions.append(led_green)
+    
+# Encoder
+encoder_map = [[(KC.VOLD,KC.VOLU,1)]]
+encoder_ext = EncoderHandler([board.GP13],[board.GP14],encoder_map)
+encoder_ext.encoders[0].is_inverted = True
+# -----
+keyboard.col_pins = (board.GP2, board.GP3, board.GP4)
+keyboard.row_pins = (board.GP6, board.GP7, board.GP8)
+keyboard.diode_orientation = DiodeOrientation.COLUMNS
+
+keyboard.keymap = [
+    [
+        KC.LCTL(KC.LSFT(KC.T)),KC.LCTL(KC.F4),KC.LCTL(KC.T),
+        KC.F11,KC.LCTL(KC.LSFT(KC.TAB)),KC.LCTL(KC.TAB),
+        KC.LWIN(KC.PSCR),KC.LEFT,KC.MPLY
+     ]
+]
+keyboard.modules = [encoder_ext]
+
+if __name__ == '__main__':
+    #led.value = True
+    keyboard.go()
+
